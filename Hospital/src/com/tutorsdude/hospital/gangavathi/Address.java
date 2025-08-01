390c92f0f03d070fa297d80c6bf18918f6829972
L Manjunatha <its.appu18@gmail.com>
2025-08-01 19:17:06 +0530

    Association Has a Relation in Java

---
+++ Hospital/src/com/tutorsdude/hospital/gangavathi/Address.java
@@ -0,0 +1,19 @@
+package com.tutorsdude.hospital.gangavathi;
+
+public class Address {
+
+    public int no;
+    public String name;
+    public String area;
+    public String city;
+    public int pincode;
+
+    public void printAddress() {
+        System.out.println("No.: " + no);
+        System.out.println("Name: " + name);
+        System.out.println("Area: " + area);
+        System.out.println("City: " + city);
+        System.out.println("Pincode: " + pincode);
+    }
+
+}
