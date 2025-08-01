ac81e219a5c98bb42017f07783bef6e19ac328bc
L Manjunatha <its.appu18@gmail.com>
2025-08-01 19:23:02 +0530

    Class Test Task On Inheritance

---
+++ InteritanceTask1/src/com/tutrousdude/inheritancetask1/multilevel/Device.java
@@ -0,0 +1,12 @@
+package com.tutrousdude.inheritancetask1.multilevel;
+
+public class Device {
+
+    public String name;
+    public String type;
+
+    public void uses(){
+        System.out.println("communication, Entertainment, online collaboration  etc....");
+    }
+
+}
---
+++ InteritanceTask1/src/com/tutrousdude/inheritancetask1/single/Computer.java
@@ -0,0 +1,12 @@
+package com.tutrousdude.inheritancetask1.single;
+
+public class Computer {
+
+    public String name;
+    public int prize;
+
+    public void applications(){
+        System.out.println("Computer are widely used to develop applications");
+    }
+
+}
